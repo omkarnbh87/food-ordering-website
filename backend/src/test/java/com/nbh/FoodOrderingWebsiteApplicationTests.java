84f0f510c3fb81d3692782a161ef173714ba7d2f
Omkarnbh87 <contact.omkar87@gmail.com>
2024-09-07 12:31:44 +0530

    Spring Boot Initial Setup done

---
+++ backend/src/test/java/com/nbh/FoodOrderingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
