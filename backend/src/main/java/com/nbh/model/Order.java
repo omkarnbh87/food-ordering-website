8888db277a46f3413a808a0c8c54d2f06a016264
Omkarnbh87 <contact.omkar87@gmail.com>
2024-09-07 16:08:56 +0530

    All models are created

---
+++ backend/src/main/java/com/nbh/model/Order.java
@@ -0,0 +1,59 @@
+package com.nbh.model;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@ManyToOne
+	private User customer;
+
+	@JsonIgnore
+	@ManyToOne
+	private Restaurant restaurant;
+	
+	private Long totalAmount;
+	
+	private String orderStatus;
+	
+	private Date createdAt;
+	
+	@ManyToOne
+	private Address deliveryAddress;
+	
+	@OneToMany
+	private List<OrderItem> items;
+	
+//	private Payment payment;
+	
+	private int totalItem;
+	
+	private int totalPrice;
+	
+	
+	
+	
+	
+}
---
+++ backend/src/main/java/com/nbh/model/User.java
@@ -0,0 +1,40 @@
+package com.nbh.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nbh.dto.RestaurantDto;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String fullName;
+	private String name;
+	private String password;
+	private USER_ROLE role;
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+	private List<Order> orders = new ArrayList<>();
+	@ElementCollection
+	private List<RestaurantDto> favourites = new ArrayList<>();
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Address> addresses = new ArrayList<>();
+}
